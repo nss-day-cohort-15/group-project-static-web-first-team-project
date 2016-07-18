c5d188e01b30eb1d3ac645f0de1ebab083f9c9f0
Casey Reed <casey.w.reed@gmail.com>
2016-07-18 13:40:18 -0500

    Products styled and tested. PNG envelope image added. Basic functionality complete

--- scripts.js
+++ scripts.js
@@ -1,65 +1,65 @@
 var comp1 = {
-image: "",
+image: "<img src='images/envelope.png'",
 name: "TEST NAME COMPLIMENT #1",
 description: "THIS IS THE DESCRIPTION FOR COMPLIMENT NUMBER 1",
 availability: "YES",
-hand: "FOREHAND"
+price: "$49.99"
 }
 
 var comp2 = {
-image: "",
+image: "<img src='images/envelope.png'",
 name: "TEST NAME COMPLIMENT #2",
 description: "THIS IS THE DESCRIPTION FOR COMPLIMENT NUMBER 2",
 availability: "YES",
-hand: "FOREHAND"
+price: "$49.99"
 }
 
 var comp3 = {
-image: "",
+image: "<img src='images/envelope.png'",
 name: "TEST NAME COMPLIMENT #3",
 description: "THIS IS THE DESCRIPTION FOR COMPLIMENT NUMBER 3",
 availability: "YES",
-hand: "FOREHAND"
+price: "$49.99"
 }
 
 var comp4 = {
-image: "",
+image: "<img src='images/envelope.png'",
 name: "TEST NAME COMPLIMENT #4",
 description: "THIS IS THE DESCRIPTION FOR COMPLIMENT NUMBER 4",
 availability: "YES",
-hand: "FOREHAND"
+price: "$49.99"
 }
 
 var insult1 = {
-image: "",
+image: "<img src='images/envelope.png'",
 name: "TEST NAME INSULT #1",
 description: "THIS IS THE DESCRIPTION FOR INSULT NUMBER 1",
 availability: "YES",
-hand: "Backhand"
+price: "$99.99"
 }
 
 var insult2 = {
-image: "",
+image: "<img src='images/envelope.png'",
 name: "TEST NAME INSULT #2",
 description: "THIS IS THE DESCRIPTION FOR INSULT NUMBER 2",
 availability: "YES",
-hand: "Backhand"
+price: "$99.99"
 }
 
 var insult3 = {
-image: "",
+image: "<img src='images/envelope.png'",
 name: "TEST NAME INSULT #3",
 description: "THIS IS THE DESCRIPTION FOR INSULT NUMBER 3",
 availability: "YES",
-hand: "Backhand"
+price: "$99.99"
 }
 
 var insult4 = {
-image: "",
+image: "<img src='images/envelope.png'",
 name: "TEST NAME INSULT #4",
 description: "THIS IS THE DESCRIPTION FOR INSULT NUMBER 4",
 availability: "YES",
-hand: "Backhand"
+price: "$99.99"
 }
 
 
@@ -68,9 +68,12 @@ var productSpecs = [comp1, comp2, comp3, comp4, insult1, insult2, insult3, insul
 function populateProducts() {
   for (i = 0; i < productSpecs.length; i++) {
     document.getElementById("products").innerHTML +=
-    "<article><h1>" + productSpecs[i].name + "</h1>" + "<p>" +productSpecs[i].description + "</p>"
+    "<article>" + productSpecs[i].image + "</img>" +
+    "<h1>" + productSpecs[i].name + "</h1>" +
+    "<p>" + productSpecs[i].description + "</p>" +
+    "<h3>" + productSpecs[i].availability + "</h3>" +
+    "<p>" + productSpecs[i].price + "</p>"
     "</article>"
-
   }
 }
 

dbdc859078453b8554953d43dba6236f20201533
Edgar Barajas <ebarajas05@gmail.com>
2016-07-20 16:30:24 -0500

    transitioned multiple page site to SPA

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
+console.log('Welcome to our website fellow developer!')
+
+var navLinks = document.querySelectorAll('li')
+var pages = document.querySelectorAll('.page')
+
+showPage(location.hash)
+
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', clickedNavLink);
+}
+
+function clickedNavLink (evt) {
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].classList.add('hidden')
+  }
+
+  showPage(evt.target.hash)
+}
+
+function showPage (id) {
+  if (id) {
+    document.querySelector(id).classList.remove('hidden')
+  } else {
+    document.querySelector('#home').classList.remove('hidden')
+  }
+}
+
+
 var comp1 = {
 image: "<img src='images/MMDlogo3.png'",
 name: "COMPLIMENT #1",
